f09a1c7a73a10074eff197615fd5bb04e34ac70e
Geetr <Geetr993@gmail.com>
2026-01-27 17:52:27 +0300

    wrote script that calculates square roots

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-import math
+import math, time
 
-number = 144
-sqrt_value = math.sqrt(number)
+start_time = time.monotonic()
+duration = 1
+number = 0
 
-print(sqrt_value)
-print(type(sqrt_value))
+with open("output_simple.txt", "w") as file:
+    while(time.monotonic() - start_time) < duration:
+        sqrt_value = math.sqrt(number)
+        number = number + 1
+        file.write(str(sqrt_value) + "\n")
+
+print("Ammount of square roots calculated in one second:", number)
